59351d9d4ccc975ca60e8c22d140e0ce4bbb54c1
David Pinto <daxtery11@gmail.com>
2020-12-24 15:31:37 +0000

    give type hints to covariance

--- trigger/clusters/covariance.py
+++ trigger/clusters/covariance.py
@@ -5,25 +5,25 @@ from scipy.spatial.distance import mahalanobis
 
 class ClusterNode:
 
-    def __init__(self, id, instance: np.ndarray, initial_std: float, dimensions: int) -> None:
+    def __init__(self, id, embedding: np.ndarray, initial_std: float, dimensions: int) -> None:
 
         self.id = id
         self.dimensions = dimensions
 
         self.cov_matrix = np.eye(dimensions)
         self.std = initial_std
-        self.mean = instance
-        self.instances = [instance]
+        self.mean = embedding
+        self.instances = [embedding]
 
-        i_observation = instance.reshape((dimensions, 1))
+        i_observation = embedding.reshape((dimensions, 1))
 
         self.observations = i_observation
 
-    def add_instance(self, instance) -> None:
+    def add_embedding(self, embedding) -> None:
 
-        self.instances.append(instance)
+        self.instances.append(embedding)
 
-        self.observations = np.hstack([self.observations, instance.reshape((self.dimensions, 1))])
+        self.observations = np.hstack([self.observations, embedding.reshape((self.dimensions, 1))])
 
         self.cov_matrix = np.cov(self.observations)
 
@@ -36,7 +36,7 @@ class ClusterNode:
 
 class CovarianceCluster(Processor):
 
-    def __init__(self, dimensions: int, initial_std=0.01) -> None:
+    def __init__(self, dimensions: int, initial_std: float = 0.01) -> None:
 
         self.initial_std = initial_std
         self.tag_to_cluster: Dict[str, int] = {}
@@ -44,47 +44,47 @@ class CovarianceCluster(Processor):
         self.clusters: Dict[int, ClusterNode] = {}
         self.dimensions = dimensions
 
-    def add_to_cluster(self, tag, instance) -> None:
+    def add_to_cluster(self, tag: str, embedding: np.ndarray) -> None:
 
         id = -1
 
         if len(self.clusters) == 0:
 
-            id = self._create_node(instance)
+            id = self._create_node(embedding)
 
         else:
 
-            distance, node = self.brute_search(instance)
+            distance, node = self.brute_search(embedding)
 
             if distance < node.std:
 
-                node.add_instance(instance)
+                node.add_embedding(embedding)
                 id = node.id
 
             else:
 
-                id = self._create_node(instance)
+                id = self._create_node(embedding)
 
         self.tag_to_cluster[tag] = id
 
-    def remove_from_cluster(self, tag) -> None:
+    def remove_from_cluster(self, tag: str) -> None:
         
         pass
 
-    def stat_distance(self, instance, node: ClusterNode) -> float:
+    def stat_distance(self, embedding: np.ndarray, node: ClusterNode) -> float:
 
-        return mahalanobis(instance, node.mean, node.cov_matrix)
+        return mahalanobis(embedding, node.mean, node.cov_matrix)
 
-    def brute_search(self, instance) -> Tuple[float, ClusterNode]:
+    def brute_search(self, embedding: np.ndarray) -> Tuple[float, ClusterNode]:
 
         nodes = list(self.clusters.values())
 
         curr_node = nodes[0]
-        distance = self.stat_distance(instance, nodes[0])
+        distance = self.stat_distance(embedding, nodes[0])
 
         for node in nodes[1:]:
 
-            c_distance = self.stat_distance(instance, node)
+            c_distance = self.stat_distance(embedding, node)
 
             if c_distance < distance:
 
@@ -93,26 +93,26 @@ class CovarianceCluster(Processor):
 
         return (distance, curr_node)
 
-    def _create_node(self, instance) -> int:
+    def _create_node(self, embedding: np.ndarray) -> int:
 
         id = self.id
         self.id += 1
 
-        new_node = ClusterNode(id, instance, self.initial_std, self.dimensions)
+        new_node = ClusterNode(id, embedding, self.initial_std, self.dimensions)
 
         self.clusters[id] = new_node
 
         return id
 
-    def process(self, tag: str, instance: np.ndarray) -> None:
+    def process(self, tag: str, embedding: np.ndarray) -> None:
 
-        self.add_to_cluster(tag, instance)
+        self.add_to_cluster(tag, embedding)
 
-    def update(self, tag: str, instance: np.ndarray) -> None:
+    def update(self, tag: str, embedding: np.ndarray) -> None:
 
         self.remove(tag)
 
-        self.process(tag, instance)
+        self.process(tag, embedding)
 
     def remove(self, tag: str) -> None:
 
@@ -134,9 +134,9 @@ class CovarianceCluster(Processor):
             in self.clusters.keys()
         ]
 
-    def predict(self, instance: np.ndarray) -> int:
+    def predict(self, embedding: np.ndarray) -> int:
 
-        return self.brute_search(instance)[1].id
+        return self.brute_search(embedding)[1].id
 
     def describe(self) -> Dict[str, Any]:
 
