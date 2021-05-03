4481cfd5d21a3d11635f1443c3857013654eb026
David Pinto <daxtery11@gmail.com>
2021-05-03 19:30:56 +0100

    simplify transformer pipelines

--- interference/transformers/transformer_pipeline.py
+++ interference/transformers/transformer_pipeline.py
@@ -1,32 +1,42 @@
 from abc import abstractmethod
 from dataclasses import dataclass, field
 from typing import Generic, TypeVar
-from typing_extensions import Protocol
 
 import numpy
 
 T = TypeVar('T')
+
 
 @dataclass
 class Instance(Generic[T]):
     value: T
     embedding: numpy.ndarray = field(repr=False)
 
-class TransformerPipeline(Protocol, Generic[T]):
+
+class TransformerPipeline(Generic[T]):
 
     @abstractmethod
-    def transform(self, value: T) -> Instance[T]:...
+    def calculate_embedding(self, value: T) -> numpy.ndarray: ...
+
+    def transform(self, value: T) -> Instance[T]:
+        embedding = self.calculate_embedding(value)
+        return Instance(value, embedding)
 
 
 class NumpyToInstancePipeline(TransformerPipeline[numpy.ndarray]):
 
-    def transform(self, numpy_array: numpy.ndarray) -> Instance[numpy.ndarray]:
-        assert isinstance(numpy_array, numpy.ndarray)
-        return Instance(numpy_array, numpy_array)
-    
+    def calculate_embedding(self, value: numpy.ndarray):
+        assert isinstance(value, numpy.ndarray)
+        return value
+
+
 class IdentityPipeline(TransformerPipeline[T]):
 
-    def transform(self, instance: Instance[T]) -> Instance[T]:
-        assert hasattr(instance, 'value')
-        assert hasattr(instance, 'embedding')
-        return instance
+    def calculate_embedding(self, value: Instance[T]) -> numpy.ndarray:
+        assert hasattr(value, 'embedding')
+        return value.embedding
+
+    def transform(self, value: Instance[T]) -> Instance[T]:
+        embedding = self.calculate_embedding(value)
+        assert hasattr(value, 'value')
+        return Instance(value.value, embedding)
